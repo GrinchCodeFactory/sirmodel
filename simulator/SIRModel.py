60565f48c4757ce4aa2124a76200285ccc7a999b
Fabian Schlieper <fabian@schlieper.email>
2020-03-29 20:39:06 +0200

    sir model rework

--- simulator/SIRModel.py
+++ simulator/SIRModel.py
@@ -1,3 +1,5 @@
+from typing import Iterable, List
+
 import Commuter
 
 
@@ -8,14 +10,25 @@ import Commuter
 ##                                                          ##
 ##############################################################
 
+def apply_sir_differential1(o, beta, gamma, ir):
+    p_sus = beta * ir
+    dR = o.I * gamma
+    o.S -= p_sus * o.S
+    o.I += p_sus * o.S - dR
+    o.R += dR
+
+
+def apply_sir_differential(o, beta, gamma, ir):
+    # p_sus = beta * ir
+    dR = o.I * gamma
+    o.S += -beta * o.S / o.N * o.I
+    o.I += beta * o.S / o.N * o.I - dR
+    o.R += dR
+
+
 class SIRModel:
     id = 0
     name = ""
-
-    N = 0
-    S = 0
-    I = 0
-    R = 0
 
     outgoingCommuters: {int: Commuter}
 
@@ -24,12 +37,12 @@ class SIRModel:
     IwC = 0
     RwC = 0
 
-    beta = 0.00001
+    beta = 1.1
     gamma = 1 / 14
 
     timeStep = 0
 
-    def __init__(self, id, N, S, I, R, commuter: {int: Commuter}, name="NO_NAME"):
+    def __init__(self, id, N, S, I, R, commuters: List[Commuter.Commuter], name="NO_NAME"):
         # print("init id: " + str(id) + ", name: "+ str(name) + " with N: " + str(N) + ", S: " + str(I) + ", I: " + str(I) + ", R: " + str(R))
         self.id = id
         self.name = name
@@ -39,86 +52,55 @@ class SIRModel:
         self.I = I
         self.R = R
 
-        self.outgoingCommuters = commuter
+        assert N == S + I + R
+
+        self.outgoingCommuters = commuters
+        for c in commuters:
+            assert c.idFrom == self.id
 
         self.timeStep = 0
 
         self.sirSeries = []
 
-    def dSus(self):
-        return - 1 * self.beta * self.IwC * self.SwC
-
-    def dInf(self):
-        return self.beta * self.IwC * self.SwC - self.gamma * self.IwC
-
-    def dRec(self):
-        return self.gamma * self.IwC
-
-    def nextTimeStep(self, commuter):
+    def nextTimeStep(self, commuter: List[Commuter.Commuter]):
 
         # imitate day and night cycle
         dayTime = self.timeStep % 2 == 0
 
-        self.includeCommuters(dayTime, commuter)
+        present = []  # commuter if dayTime else self.outgoingCommuters
 
-        # calculating the "ableitung" for the total number of ppl
-        dS = self.dSus()
-        dI = self.dInf()
-        dR = self.dRec()
+        # self.includeCommuters(present)
 
-        # distribution of dS/dI/dR on basis vaules
-        self.S = max(self.S + (self.N / self.NwC) * dS, 0)
-        self.I = max(self.I + (self.N / self.NwC) * dI, 0)
-        self.R = max(self.R + (self.N / self.NwC) * dR, 0)
+        # if self.NwC:
+        ir = self.I / self.N  # infection ratio TODO *wC
 
+        apply_sir_differential(self, beta=self.beta, gamma=self.gamma, ir=ir)
+
+        # for c in present:
+        #    apply_sir_differential(c, beta=self.beta, gamma=self.gamma, ir=ir)
+
+        # capture SIR for plotting
         self.sirSeries.append((self.S, self.I, self.R))
-
-        commie = commuter
-        if not dayTime:
-            commie = self.outgoingCommuters.values()
-        # distribution of dS/dI/dR on commuters
-        for c in commie:
-            c.S = max(c.S + (c.N / self.NwC) * dS, 0)
-            c.I = max(c.I + (c.N / self.NwC) * dI, 0)
-            c.R = max(c.R + (c.N / self.NwC) * dR, 0)
 
         self.timeStep += 1
 
-        return commuter
-
     # add commuters from outside or add comuters coming home
-    def includeCommuters(self, workingTime: bool, commuter: [Commuter]):
+    def includeCommuters(self, commuter: Iterable[Commuter.Commuter]):
         self.NwC = self.N
         self.SwC = self.S
         self.IwC = self.I
         self.RwC = self.R
 
-        # during the day the incoming comuters are considered
-        if workingTime:
-            for cIn in commuter:
-                self.NwC += cIn.N
-                self.SwC += cIn.S
-                self.IwC += cIn.I
-                self.RwC += cIn.R
-        # during the night the outgoing comuters coming back are considered
-        else:
-            for key in self.outgoingCommuters:
-                self.NwC += self.outgoingCommuters[key].N
-                self.SwC += self.outgoingCommuters[key].S
-                self.IwC += self.outgoingCommuters[key].I
-                self.RwC += self.outgoingCommuters[key].R
+        for cIn in commuter:
+            self.NwC += cIn.N
+            self.SwC += cIn.S
+            self.IwC += cIn.I
+            self.RwC += cIn.R
 
     ##########################Setter and toString
 
-    def setOutgoingCommuter(self, oCom: Commuter):
-        self.outgoingCommuters[oCom.idTo] = oCom
-
     def setBeta(self, beta):
         self.beta = beta
-        print("set beta to :" + str(self.beta))
-
-    def setBeta(self, avgContactPerPerson, chanceToInfect):
-        self.beta = avgContactPerPerson * chanceToInfect
         print("set beta to :" + str(self.beta))
 
     def setGamma(self, gamma):
@@ -129,19 +111,22 @@ class SIRModel:
         self.gamma = 1 / duration
         print("set gamma to :" + str(self.gamma))
 
-    def setGamma(self):
+    def computeGamma(self):
         self.gamma = self.R / self.I
         print("set gamma to :" + str(self.gamma))
 
     def plot(self):
         import pandas as pd
         import matplotlib.pyplot as plt
-        pd.DataFrame(self.sirSeries, columns=['S','I','R']).plot()
+        df = pd.DataFrame(self.sirSeries, columns=['S', 'I', 'R'])
+        df.plot()
+        df.sum(axis=1).plot(label='N')
+        plt.axhline(0, c='k')
+        plt.grid()
         plt.title(self.name)
 
-
     def __str__(self):
-        msg = "SIRModel " + str(self.name) + " at t=" + str(self.timeStep/2) + "\n"
+        msg = "SIRModel " + str(self.name) + " at t=" + str(self.timeStep / 2) + "\n"
         msg += "Population: " + str(self.N) + "\n"
         msg += "Suseptible: " + str(self.S) + "P(S): " + str(self.S / self.N) + "\n"
         msg += "Infected: " + str(self.I) + "P(I): " + str(self.I / self.N) + "\n"
